a00ed44c668f7543372ec1168f104f0fa1a37925
Neha Patel <nehapatel0393@gmail.com>
2019-07-02 23:50:58 -0500

    zipcode

--- Lesson01/scripts.js
+++ Lesson01/scripts.js
@@ -14,3 +14,17 @@ $(function() {
     alert(`Email is ${$(this).siblings().val().includes("@")`});
   })
 });
+
+$(function() {
+  //Names button and click event
+
+  $(".zipcode-submit").click(function() {
+    zipcode = $(".zipcode");
+    const number = zipcode.val();
+    const result = array1.filter(number.length > 1000);
+    //Alerts words found
+
+    alert(result);
+
+  })
+});
