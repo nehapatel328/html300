4905033d1f8add2bf4ac0fe9955c459eab308fa0
Neha Patel <nehapatel0393@gmail.com>
2019-07-07 21:54:06 -0500

    gulp file

--- Lesson01/scripts.js
+++ Lesson01/scripts.js
@@ -1,3 +1,7 @@
+//FEEDBACK: Check your backticks (``) in your template strings, make sure they are within the closing parenthesis if they are a parameter.
+
+//You've used document ready declarations for each instance of your functionality, but you'll be fine with just one wrapping document ready block for all of your code.
+
 //Name: Says hello via string interpolation
 $(function() {
   $(".name-submit").click(function() {
@@ -19,13 +23,15 @@ $(function() {
   $(".zipcode-submit").click(function() {
     zipcode = $(".zipcode-submit");
     const number = zipcode.val();
-    const result = array1.filter(number.length > 1000);
+    const result = zipcode.filter(number.length > 1000);
     //Alerts words found
 
     alert(result);
 
   })
 });
+
+//In your zipcode function, you've got an array1 that doesn't exist and you'll want tot check the syntax on using filter.
 
 //Lucky number: playing around with Codepen example in manipulating the input value via reduce
 $(function () {
@@ -37,3 +43,5 @@ $(function () {
 
         return sum + value;
 });
+
+//You'll also want to check the lucky number entry as the sum variable doesn't exist, and you can't return from a non-running function.
